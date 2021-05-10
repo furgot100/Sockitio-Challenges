068472bfc43cffb850320d21b2824531b271a368
Francis Tsang <frtsang40@gmail.com>
2021-05-10 12:11:29 -0700

    restructured files

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
+// File directory
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname, 'public');
 });
 
+// Chatroom
 io.on('connection', (socket) => {
     console.log('a user connected');
     
@@ -23,10 +28,3 @@ io.on('connection', (socket) => {
       io.emit('chat message', msg);
     });
 })
-
-
-
-
-server.listen(3000, () => {
-  console.log('listening on *:3000');
-});
